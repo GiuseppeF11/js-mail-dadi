a28885498493c818d8f22d30694eb87ab390ecec
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-06 19:43:27 +0100

    Aggiunto flusso e struttura JS - EMAIL

--- js-mail/js/scripts.js
+++ js-mail/js/scripts.js
@@ -1,3 +1,30 @@
-// Test funzionamento
+// JS MAIL
 
-console.log('Template html css js')
+/* 
+1) Creare input per mettere myEmail
+2) Creare lista (array) con elenco delle emailValid
+3) SE myEmail è contenuto in emailValid
+--- stampa 'puoi entrare'
+4)ALTRIMENTi 'non puoi entrare'
+
+*/
+
+const sendButton = document.getElementById('sendButton');
+
+const emailValid = ['forza@inter.com' , 'forza@milan.com' , 'forza@napoli.com', 'forza@lazio.com'];
+console.log(emailValid);
+
+sendButton.addEventListener ('click', function() {
+    const myEmail = document.querySelector('input').value;
+    console.log('myEmail ', myEmail, typeof myEmail);
+
+    const correct = emailValid.includes(myEmail);
+
+    if (correct){
+        console.log('PUOI ENTRARE')
+    }
+    
+    else {
+        console.log('NON PUOI ENTRARE')
+    }
+})
